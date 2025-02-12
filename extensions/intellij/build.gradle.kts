e91be9aaf54ec6cfc35896582f4f39c9756926a1
exetroncorp <117201867+exetroncorp@users.noreply.github.com>
2025-02-12 22:26:56 +0000

    sss

--- extensions/intellij/build.gradle.kts
+++ extensions/intellij/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     testImplementation("com.intellij.remoterobot:remote-fixtures:$remoteRobotVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-    testImplementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.automation-remarks:video-recorder-junit5:2.0")
 
 
